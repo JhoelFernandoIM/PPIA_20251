1b9441a98facc771eb47c6a7bee20801d917c7c6
JhoelFernandoIM <i2512152@continental.edu.pe>
2025-02-20 22:13:16 -0500

    creando entorno virtual streamlit, y codigos de secuencias

---
+++ semana2.py
@@ -0,0 +1,21 @@
+import streamlit as st
+from algoritmos import AlgoritmosSecuenciales
+
+st.title("Algortimos Secuenciales con POO en Streamlit")
+
+#Entrada de usuario
+numero = st.number_input("Ingrese un número entero positivo", min_value=1, value=5, step=1)
+
+# Instanciamos la clase con el número ingresado
+
+algoritmos = AlgoritmosSecuenciales(numero)
+
+#Botones para ejecutar los algoritmos secuenciales
+if st.button("Clacular Suma de N números"):
+    st.success(f"La suma de los primeros {numero} es:{algoritmos.suma_n_numeros()} ")
+
+if st.button("Calcular  Factorial"):
+    st.success(f" El factorial de {numero} es: {algoritmos.factorial}")
+
+if st.button("Generar secuencia de fibonacci"):
+    st.success(f"La secuencia de fibonacci con {numero} terminos es: { algoritmos.fibonacci}")
