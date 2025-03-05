5d96bc493741787551028cf1d8702c81e985fff5
JhoelFernandoIM <i2512152@continental.edu.pe>
2025-03-04 21:36:34 -0500

    creando el archivo de semana4

---
+++ semana4.py
@@ -0,0 +1,11 @@
+import stramlit as st
+
+st.title("Estructura de control repetitivas")
+
+#Entrada del usuario para límites del bucle
+limite = st.number_import("Ingrese un número límite para los búcles: ", min_value=1, step 1)
+
+st.subheader("Bucle FOR")
+
+for i in range(1,limite + 1)
+st.write(f"Iteración {i} con for")
