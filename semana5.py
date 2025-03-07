592c41c039704e290b365cbb607f0c517cd5057a
JhoelFernandoIM <i2512152@continental.edu.pe>
2025-03-06 22:51:28 -0500

    v2 merge sesion5 sin eliminar archivos main

---
+++ semana5.py
@@ -0,0 +1,16 @@
+import streamlit as st
+
+def main():
+    st.title("Ejemplo de while y for en streamlit")
+
+    #Ejemplo de while
+    st.subheader("Ejemplo de while")
+
+    n = st.number_input("Ingresa un número para contar hasta 5: ", min_value=0, max_value=5)
+    contador = 0
+    resultado_while=""
+
+    while contador <=n:
+        resultado_while +=f"{contador}"
+        contador +=1
+    st.write("Secuencia generada con while: ", resultado_while)
